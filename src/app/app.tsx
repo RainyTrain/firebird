4c0987a1650cd2ad103d997bf8da9154df7a6d8a
Adrian Senkevich <adrianscorp@gmail.com>
2023-11-09 16:12:06 +0100

    addd react for application and created basic logic for error boundary

---
+++ src/app/app.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="app">blablabla</div>;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,6 @@
-console.log('hello, world')
+import App from 'app/app';
+import { createRoot } from 'react-dom/client';
+import './app/styles/index.scss';
+
+const root = createRoot(document.getElementById('root')!);
+root.render(<App />);
